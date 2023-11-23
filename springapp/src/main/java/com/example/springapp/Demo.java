9ac6251e2fec70bc141f5506c1b5ea0a61affc2d
sunilswizy <sunilsubash006@gmail.com>
2023-11-23 18:53:40 +0000

    master

--- springapp/src/main/java/com/example/springapp/Demo.java
+++ springapp/src/main/java/com/example/springapp/Demo.java
@@ -3,5 +3,6 @@ package com.example.springapp;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("haoi");
+        System.out.println("welcome");
     }
 }
